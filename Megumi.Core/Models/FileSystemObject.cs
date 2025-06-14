39b868b4098d8749d3733ff3865f4f5660428364
Victor Fugazzotto <victorfuga@gmail.com>
2025-06-14 06:50:28 -0400

    More window design

---
+++ Megumi.Core/Models/FileSystemObject.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Media.Imaging;
+using ReactiveUI;
+
+namespace Megumi.Core.Models;
+
+public class FileSystemObject : ReactiveObject
+{
+    public string Name { get; set; }
+    
+    public string Path { get; set; }
+    
+    public Bitmap Icon { get; set; }
+}
